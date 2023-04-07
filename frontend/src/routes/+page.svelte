18dd993195bb975658f86c7c23e921cd2ebc0dab
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-08 03:51:52 +0900

    SvelteKit page test

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,9 +3,8 @@
     export let data: PageData;
 </script>
   
-<!-- <h1>{data.Items.title}</h1> -->
 <ul>
-	{#each data.items as item}
+    {#each data.items as item}
         <li>{item}</li>
-	{/each}
+    {/each}
 </ul>
--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,8 +1,24 @@
 import type { PageLoad } from './$types';
  
 export const load = (async () => {
-    const res = await fetch('http://localhost:5000/test');
-    const data = await res.json();
 
-    return data;
+
+    const url = 'http://localhost:5000/login';
+    const data = { userId: 1 };
+
+    fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+    const res = await fetch('http://localhost:5000/test');
+    const data2 = await res.json();
+
+    return data2;
 }) satisfies PageLoad;
