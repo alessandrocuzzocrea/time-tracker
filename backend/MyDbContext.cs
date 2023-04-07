afdbbaf33e581f1192267486bd504f98230069e5
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-08 04:27:17 +0900

    Add context ctor

--- backend/MyDbContext.cs
+++ backend/MyDbContext.cs
@@ -4,6 +4,10 @@ public class MyDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
 
+    public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+    {
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=mydatabase.db");
