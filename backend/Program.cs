b640ae3a5d88fd1054f11ab7e1b2df802c4ff88f
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-08 03:50:47 +0900

    Disable UseAuthorization

--- backend/Program.cs
+++ backend/Program.cs
@@ -34,7 +34,7 @@ if (app.Environment.IsDevelopment())
 
 // app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapGet("/test", () => {
     var result = new { Items = new [] { "1", "2", "3" } };
