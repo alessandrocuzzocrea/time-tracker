a18b6a2424fec51cb2e0f6254f03a82f0c03dc60
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-12 01:51:10 +0900

    ChangeTimeEntriesTableDescriptionColumnToAllowNull

--- backend/Models/TimeEntry.cs
+++ backend/Models/TimeEntry.cs
@@ -3,7 +3,7 @@ public class TimeEntry : ITimestampedEntity
     public int Id { get; set; }
     public int TaskId { get; set; }
     // public int UserId { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime? EndTime { get; set; }
 
