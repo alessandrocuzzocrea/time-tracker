a72b2f3e1ee24f58d301ca808e860f0ed2c9e72b
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-12 01:54:52 +0900

    AddReferenceToUserIdToTimeEntriesTable

---
+++ backend/Migrations/20230411065329_AddReferenceToUserIdToTimeEntriesTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReferenceToUserIdToTimeEntriesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "TimeEntries",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeEntries_UserId",
+                table: "TimeEntries",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TimeEntries_AspNetUsers_UserId",
+                table: "TimeEntries",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TimeEntries_AspNetUsers_UserId",
+                table: "TimeEntries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TimeEntries_UserId",
+                table: "TimeEntries");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "TimeEntries");
+        }
+    }
+}
--- backend/Models/TimeEntry.cs
+++ backend/Models/TimeEntry.cs
@@ -2,13 +2,13 @@ public class TimeEntry : ITimestampedEntity
 {
     public int Id { get; set; }
     public int TaskId { get; set; }
-    // public int UserId { get; set; }
+    public int UserId { get; set; }
     public string? Description { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime? EndTime { get; set; }
 
     public Task Task { get; set; } = null!;
-    // public ApplicationUser User { get; set; } = null!;
+    public ApplicationUser User { get; set; } = null!;
 
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
