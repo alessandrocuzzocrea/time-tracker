c89fb9373aaa6e1bade5a104808662cc6cbfb575
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-12 21:35:05 +0900

    wip-3

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -32,7 +32,7 @@
 					</li>
 					<li>
 						<a
-							href="/browse"
+							href="#"
 							class="flex items-center space-x-2 py-2 px-3 rounded-lg hover:bg-blue-700 transition-colors"
 						>
 							<!-- <span class="material-icons">inbox</span> -->
