7cb3a57369982f7399f1791d3cfa251e95b9a0fd
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-14 00:15:15 +0900

    Icon

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -9,25 +9,22 @@
 						placeholder="Search..."
 					/>
 					<svg
+						class="h-6 w-6 text-blue-400 mr-4 -ml-12"
+						fill="#AB7C94"
+						width="800px"
+						height="800px"
+						viewBox="0 0 32 32"
+						enable-background="new 0 0 32 32"
+						id="Glyph"
+						version="1.1"
+						xml:space="preserve"
 						xmlns="http://www.w3.org/2000/svg"
-						class="h-6 w-6 text-gray-400 mr-4 -ml-8"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
+						xmlns:xlink="http://www.w3.org/1999/xlink"
+						><path
+							d="M27.414,24.586l-5.077-5.077C23.386,17.928,24,16.035,24,14c0-5.514-4.486-10-10-10S4,8.486,4,14  s4.486,10,10,10c2.035,0,3.928-0.614,5.509-1.663l5.077,5.077c0.78,0.781,2.048,0.781,2.828,0  C28.195,26.633,28.195,25.367,27.414,24.586z M7,14c0-3.86,3.14-7,7-7s7,3.14,7,7s-3.14,7-7,7S7,17.86,7,14z"
+							id="XMLID_223_"
+						/></svg
 					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M21 21l-4.35-4.35"
-						/>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M15.5 10.5a5 5 0 115 5 5 5 0 01-5-5z"
-						/>
-					</svg>
 				</div>
 			</form>
 		</div>
