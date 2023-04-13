58329f34f392eb477443d8c82790abf9e2db0003
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-14 02:14:43 +0900

    wow

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,6 +3,12 @@
 	import { GlobeAlt, CheckCircle, Rss, CalendarDays, ChartPie, Folder } from 'svelte-heros';
 	import Player from '../components/Player.svelte';
 	import Header from '../components/Header.svelte';
+
+	import { fly, fade } from 'svelte/transition';
+	let showSidePanel, width;
+	function toggleSidePanel() {
+		showSidePanel = !showSidePanel;
+	}
 </script>
 
 <body class="bg-gray-200">
@@ -11,6 +17,9 @@
 		<div class="bg-electric-indigo w-48 flex flex-col p-4 text-gray-custom-1">
 			<div class="mb-8">
 				<h1 class="text-2xl font-semibold mb-2">TimeTracker</h1>
+			</div>
+			<div>
+				<button class="p-3" on:click={toggleSidePanel}> Show side panel </button>
 			</div>
 			<nav>
 				<p class="text-xs mb-2">TRACK</p>
@@ -98,6 +107,45 @@
 			</div>
 		</div>
 	</div>
+	{#if showSidePanel}
+		<aside class="relative z-10">
+			<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" transition:fade />
+			<div class="fixed inset-0 overflow-hidden">
+				<div class="absolute inset-0 overflow-hidden">
+					<div class="absolute inset-y-0 right-0 mt-20 flex max-w-full pl-10">
+						<div class="my-auto h-full w-screen max-w-2xl">
+							<div
+								bind:clientWidth={width}
+								transition:fly={{ x: width }}
+								class="m-auto flex h-[95%] flex-col overflow-y-scroll bg-white py-6 shadow-xl"
+							>
+								<div class="px-4 sm:px-6">
+									<div class="flex items-start justify-between">
+										<h2 class="text-lg font-medium text-gray-900">Panel title</h2>
+										<div class="ml-3 flex h-7 items-center">
+											<button
+												class="rounded-md bg-white font-bold text-gray-400 hover:text-gray-500"
+												on:click={toggleSidePanel}
+											>
+												close
+											</button>
+										</div>
+									</div>
+								</div>
+								<div class="relative mt-6 flex-1 px-4 sm:px-6">
+									<div class="absolute inset-0 px-4 sm:px-6">
+										<div class="h-full border-2 border-dashed border-gray-200" aria-hidden="true">
+											Some content
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</aside>
+	{/if}
 </body>
 
 <style>
