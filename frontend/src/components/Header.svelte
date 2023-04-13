f0e4b13841e4c698b6dffa8c4f5a5e717519f489
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-14 00:50:42 +0900

    avi

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -1,10 +1,14 @@
+<script>
+	import avatar1 from '$lib/assets/1.jpg';
+</script>
+
 <header class="bg-white text-gray-800 py-4">
 	<div class="container mx-auto flex justify-between items-center px-4">
 		<div class="flex items-center flex-grow">
 			<form class="w-full">
-				<div class="flex items-center">
+				<div class="flex items-center mr-16">
 					<input
-						class="bg-gray-100 text-gray-800 border border-gray-200 py-2 px-4 rounded-l-full rounded-r-full w-full focus:outline-none focus:ring focus:border-blue-300"
+						class="bg-gray-100 text-gray-800 border border-gray-200 py-2 px-4 rounded-l-full rounded-r-full block w-full focus:outline-none focus:ring focus:border-blue-300"
 						type="text"
 						placeholder="Search..."
 					/>
@@ -30,25 +34,42 @@
 		</div>
 		<div class="flex items-center">
 			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="h-6 w-6 text-gray-800 mr-4"
+				class="h-12 w-12 text-blue-400 mr-8 -mb-3"
 				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
+				width="800px"
+				height="800px"
+				viewBox="0 0 32 32"
+				enable-background="new 0 0 32 32"
+				id="Glyph"
+				version="1.1"
+				xml:space="preserve"
+				xmlns="http://www.w3.org/2000/svg"
+				xmlns:xlink="http://www.w3.org/1999/xlink"
 			>
 				<path
+					d="M12.02 2.90991C8.70997 2.90991 6.01997 5.59991 6.01997 8.90991V11.7999C6.01997 12.4099 5.75997 13.3399 5.44997 13.8599L4.29997 15.7699C3.58997 16.9499 4.07997 18.2599 5.37997 18.6999C9.68997 20.1399 14.34 20.1399 18.65 18.6999C19.86 18.2999 20.39 16.8699 19.73 15.7699L18.58 13.8599C18.28 13.3399 18.02 12.4099 18.02 11.7999V8.90991C18.02 5.60991 15.32 2.90991 12.02 2.90991Z"
+					stroke="#292D32"
+					stroke-width="1.5"
+					stroke-miterlimit="10"
+					stroke-linecap="round"
+				/>
+				<path
+					d="M13.87 3.19994C13.56 3.10994 13.24 3.03994 12.91 2.99994C11.95 2.87994 11.03 2.94994 10.17 3.19994C10.46 2.45994 11.18 1.93994 12.02 1.93994C12.86 1.93994 13.58 2.45994 13.87 3.19994Z"
+					stroke="#292D32"
+					stroke-width="1.5"
+					stroke-miterlimit="10"
 					stroke-linecap="round"
 					stroke-linejoin="round"
-					stroke-width="2"
-					d="M15 19a2 2 0 01-2 2H7a2 2 0 01-2-2V9a2 2 0 012-2h6m3-3v3m0 0v3m0-3h3m-3 0H9a2 2 0 00-2 2v8a2 2 0 002 2h6a2 2 0 002-2V9a2 2 0 00-2-2z"
+				/>
+				<path
+					d="M15.02 19.0601C15.02 20.7101 13.67 22.0601 12.02 22.0601C11.2 22.0601 10.44 21.7201 9.90002 21.1801C9.36002 20.6401 9.02002 19.8801 9.02002 19.0601"
+					stroke="#292D32"
+					stroke-width="1.5"
+					stroke-miterlimit="10"
 				/>
 			</svg>
 			<div class="flex items-center">
-				<img
-					class="h-12 rounded-full mr-4"
-					src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-					alt="Profile"
-				/>
+				<img class="h-12 rounded-full mr-4" src={avatar1} alt="Profile" />
 				<div class="mr-4">
 					<div class="font-bold">Pompompurin</div>
 					<div class="text-sm text-gray-500">Team Sanrio</div>
