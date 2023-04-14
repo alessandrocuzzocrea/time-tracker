5ffbbaf096584bfae4d9b760b174cc1b1e3bd956
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-15 00:21:05 +0900

    icon

--- frontend/src/lib/components/HomeScreenTaskTile.svelte
+++ frontend/src/lib/components/HomeScreenTaskTile.svelte
@@ -1,6 +1,20 @@
 <div class="w-64 h-48 bg-white rounded-3xl flex flex-col overflow-hidden">
-	<div class="flex-initial w-64 h-24 bg-blue-400" />
-	<div class="flex-initial font-xl">🖥</div>
+	<div class="flex-initial w-64 h-32 bg-blue-400" />
+	<div class="flex justify-center relative">
+		<img
+			alt="..."
+			src="https://demos.creative-tim.com/notus-js/assets/img/team-2-800x800.jpg"
+			class="shadow-xl rounded-full h-12 align-middle border-none absolute -m-6 max-w-150-px"
+		/>
+	</div>
+
 	<div class="w-full h-full bg-blue">TEST</div>
 	<div class="w-full h-full bg-blue">TES2</div>
 </div>
+
+<style>
+	span {
+		font-size: 48px;
+		width: 26px;
+	}
+</style>
