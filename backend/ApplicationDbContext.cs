52a1fd3c000d280f4b330ebacf391d9fe1beef96
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-15 23:30:22 +0900

    seed

--- backend/ApplicationDbContext.cs
+++ backend/ApplicationDbContext.cs
@@ -5,11 +5,41 @@ using Microsoft.EntityFrameworkCore;
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
 {
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
-     public DbSet<TimeEntry> TimeEntries { get; set; }
+    public DbSet<TimeEntry> TimeEntries { get; set; }
     public DbSet<Task> Tasks { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Task>().HasData(
+            new Task
+            {
+                Id = 1,
+                Name = "Implement database for user authentication",
+                ProjectId = 2
+            },
+            new Task
+            {
+                Id = 2,
+                Name = "Prepare API docs",
+                ProjectId = 1
+            },
+            new Task
+            {
+                Id = 3,
+                Name = "Build login functionality",
+                ProjectId = 2
+            }
+        );
+        // modelBuilder.Entity<Book>().HasData(
+        //     new Book { BookId = 1, AuthorId = 1, Title = "Hamlet" },
+        //     new Book { BookId = 2, AuthorId = 1, Title = "King Lear" },
+        //     new Book { BookId = 3, AuthorId = 1, Title = "Othello" }
+        // );
+        base.OnModelCreating(modelBuilder);
     }
 
     // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
---
+++ backend/Migrations/20230415142541_Seed.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TimeTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "Id", "CreatedAt", "Name", "ProjectId", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Implement database for user authentication", 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Prepare API docs", 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Build login functionality", 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
