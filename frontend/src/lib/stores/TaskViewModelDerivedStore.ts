0512d67a5cb59a290b9738a399d904d1795a7f6d
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-21 22:03:21 +0900

    rename TaskViewModelDerivedStore

---
+++ frontend/src/lib/stores/TaskViewModelDerivedStore.ts
@@ -0,0 +1,27 @@
+import { derived } from 'svelte/store';
+import { ProjectStore } from './ProjectStore';
+import { TaskStore } from './TaskStore';
+import { UserStore } from './UserStore';
+import type { Task } from './TaskStore';
+import type { User } from './UserStore';
+
+export const TaskViewModelDerivedStore = derived(
+  [UserStore, ProjectStore, TaskStore],
+  ([$UserStore, $ProjectStore, $TaskStore]) => {
+    console.log('TaskViewModelDerivedStore update');
+    return $TaskStore.map((task: Task) => {
+      const owner = UserStore.findById(task.ownerId);
+      const project = ProjectStore.findById(task.projectId);
+
+      return {
+        projectId: project?.id,
+        projectName: project?.projectName,
+        projectColor: project?.projectColor,
+        taskId: task.id,
+        taskName: task.name,
+        ownerName: owner?.name,
+        ownerPic: owner?.avi
+      };
+    });
+  }
+);
