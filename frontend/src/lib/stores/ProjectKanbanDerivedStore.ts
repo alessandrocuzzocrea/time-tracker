bbd1c05dc58515ef8cd485176772e9d8b40b41ba
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-27 00:02:35 +0900

    ll

--- frontend/src/lib/stores/ProjectKanbanDerivedStore.ts
+++ frontend/src/lib/stores/ProjectKanbanDerivedStore.ts
@@ -9,7 +9,6 @@ export const ProjectKanbanDerivedStore = derived(
   ([$page, $ProjectStore, $TaskStore]) => {
     return $TaskStore.filter(task => task.projectId === parseInt($page.params.id, 10)).map((task) => {
       const project = ProjectStore.findById(task?.projectId);
-      // debugger;
       return {
         taskId: task.id,
         projectId: task?.projectId,
@@ -21,21 +20,3 @@ export const ProjectKanbanDerivedStore = derived(
     });
   }
 );
-
-// export const ProjectKanbanDerivedStore = derived(
-//   [ProjectStore, TaskStore],
-//   ([$ProjectStore, $TaskStore]) => {
-//     return $TaskStore.map((task) => {
-//       const project = ProjectStore.findById(task?.projectId);
-
-//       return {
-//         taskId: task.id,
-//         projectId: task?.projectId,
-//         projectName: project?.projectName,
-//         projectColor: project?.projectColor,
-//         taskName: task?.name,
-//         status: task?.status
-//       };
-//     });
-//   }
-// );
