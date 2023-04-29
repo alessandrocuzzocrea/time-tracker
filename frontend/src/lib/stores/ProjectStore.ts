9041d810a7c61207135280f4c5e61baea105054e
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2023-04-30 00:02:39 +0900

    restful store

--- frontend/src/lib/stores/ProjectStore.ts
+++ frontend/src/lib/stores/ProjectStore.ts
@@ -7,31 +7,59 @@ export type Project = {
 };
 
 function createProjectStore() {
-  const projects: Project[] = [
-    {
-      id: 1,
-      projectName: 'Project 1',
-      projectColor: 'red'
-    },
-    {
-      id: 2,
-      projectName: 'Project 2',
-      projectColor: 'blue'
-    }
-  ];
-
-  // const store = writable(projects);
   const store = writable<Project[]>([]);
-  const { subscribe, set } = store;
+  const { subscribe, set, update } = store;
+
+  async function loadProjects() {
+    const response = await fetch('/api/projects');
+    const data = await response.json();
+    set(data);
+  }
+
+  async function createProject(project: Project) {
+    const response = await fetch('/api/projects', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    });
+    const data = await response.json();
+    update(projects => [...projects, data]);
+  }
+
+  async function updateProject(project: Project) {
+    const response = await fetch(`/api/projects/${project.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    });
+    const data = await response.json();
+    update(projects => projects.map(p => (p.id === data.id ? data : p)));
+  }
+
+  async function deleteProject(project: Project) {
+    const response = await fetch(`/api/projects/${project.id}`, {
+      method: 'DELETE'
+    });
+    if (response.ok) {
+      update(projects => projects.filter(p => p.id !== project.id));
+    }
+  }
 
   function findById(id: number) {
     return get(store).find((e) => e.id === id);
   }
 
-  setTimeout(() => set(projects), 0);
+  setTimeout(loadProjects, 0);
 
   return {
     subscribe,
+    createProject,
+    updateProject,
+    deleteProject,
     findById
   };
 }
